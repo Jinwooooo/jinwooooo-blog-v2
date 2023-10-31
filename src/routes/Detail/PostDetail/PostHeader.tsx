3f3cd7c8da44bf741e65a22cce16fe83775e2a38
Jinwooooo <chungjinwoo5d@naver.com>
2023-10-31 15:22:53 +0900

    Revert "[test] shortDate"
    This reverts commit 62d077b76617282963be1a1b3f8b983e99c08d23.

--- src/routes/Detail/PostDetail/PostHeader.tsx
+++ src/routes/Detail/PostDetail/PostHeader.tsx
@@ -32,7 +32,12 @@ const PostHeader: React.FC<Props> = ({ data }) => {
                 <div className="hr"></div>
               </>
             )}
-            <div className="date">{data.shortDate}</div>
+            <div className="date">
+              {formatDate(
+                data?.date?.start_date || data.createdTime,
+                CONFIG.lang
+              )}
+            </div>
           </div>
           <div className="mid">
             {data.tags && (
